fc1dd6177384e74eb4f9999e22600386b11f705a
kishanrampure <kishan14@amityonline.com>
2024-04-19 01:12:55 +0530

    Edited Files

--- EKS/main.tf
+++ EKS/main.tf
@@ -36,14 +36,14 @@ module "eks" {
   cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = var.private_subnets
+  subnet_ids                     = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     nodes = {
-      min_size      = 2
-      max_size      = 10
-      desired_size  = 2
-      instance_type = var.instance_type
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = var.instance_types
     }
   }
 
--- EKS/variables.tf
+++ EKS/variables.tf
@@ -4,16 +4,16 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnets" {
-  description = "public_subnet"
+  description = "public_subnets CIDR"
   type        = list(string)
 }
 
 variable "private_subnets" {
-  description = "Private_subnet CIDR"
+  description = "private_subnets CIDR"
   type        = list(string)
 }
 
-variable "instance_type" {
-  description = "Node Instance"
+variable "instance_types" {
+  description = "Node Instances"
   type        = list(string)
 }
